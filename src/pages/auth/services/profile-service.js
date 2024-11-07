75bd21f4a4e11389e1417f39b11ca7f3df7299e8
gclsistemas <gclsistemas@outlook.com>
2024-11-07 19:40:08 -0300

    Register Page

--- src/pages/auth/services/profile-service.js
+++ src/pages/auth/services/profile-service.js
@@ -5,4 +5,4 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const profileUpdate = async (id, info) => {
     const config = { headers: { 'Content-Type': 'application/json' } };
     return await axios.post(`${API_URL}/user/${id}/update`, JSON.stringify(info), config);
-}
+};
